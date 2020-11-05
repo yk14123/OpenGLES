72ccb75daa580842b3149ec984ae9357860b0a98
yk14123 <279461375@qq.com>
2020-11-05 23:31:44 +0800

    init 第5章

---
+++ airhockey5/src/main/java/com/chinafocus/airhockey5/AirHockeyActivity.java
@@ -0,0 +1,55 @@
+package com.chinafocus.airhockey5;
+
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.pm.ConfigurationInfo;
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AirHockeyActivity extends AppCompatActivity {
+
+    private GLSurfaceView glSurfaceView;
+    private boolean rendererSet = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        glSurfaceView = new GLSurfaceView(this);
+
+        final ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        final ConfigurationInfo deviceConfigurationInfo = activityManager.getDeviceConfigurationInfo();
+        final boolean supportsEs2 = deviceConfigurationInfo.reqGlEsVersion >= 0x20000;
+        glSurfaceView.setEGLConfigChooser(8, 8, 8, 8, 16, 0);
+        if (supportsEs2) {
+            glSurfaceView.setEGLContextClientVersion(2);
+            glSurfaceView.setRenderer(new AirHockeyRenderer(this));
+            rendererSet = true;
+        } else {
+            Toast.makeText(this, "This device does not support OpenGL ES 2.0.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        setContentView(glSurfaceView);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (rendererSet) {
+            glSurfaceView.onResume();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (rendererSet) {
+            glSurfaceView.onPause();
+        }
+    }
+}
